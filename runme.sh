7905f2ea29a40ef4a10070eca3ad0b81f60f33a1
Abhishek Malani <malani@gmail.com>
2024-11-07 10:32:45 -0800

    Update runme with command line

--- runme.sh
+++ runme.sh
@@ -4,6 +4,6 @@ echo "Pulling down latest git repo changes"
 git pull
 
 source ./venv/bin/activate
-sudo python3 ./discovery.py
+sudo python3 ./discovery.py -u
 truncate -s 100K ./cron_log.log
 sudo pihole restartdns
